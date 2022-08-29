c9868e0c55334c3d2e3be4c6244baf460164c0ef
Jihun Song <songjihun940409@gmail.com>
2022-08-29 15:54:49 +0900

    1.11 Droppable Snapshot

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { DragDropContext, Droppable, DropResult } from "@hello-pangea/dnd";
+import { DragDropContext, DropResult } from "@hello-pangea/dnd";
 import { useRecoilState } from "recoil";
 import styled from "styled-components";
 import { toDoState } from "./atoms";
